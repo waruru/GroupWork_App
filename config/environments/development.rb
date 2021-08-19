a3d0f27fb743b4e538349d3259411ec6bddeccc2
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-08-19 14:48:10 +0900

    devise メール認証設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_name: ENV['MAILER_USER'],
+    password: ENV['MAILER_PASS'],
+    authentication: 'login'
+  }
 end
