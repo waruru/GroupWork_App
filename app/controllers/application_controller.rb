fb14df217228a7b504c1603ef5b09fac93eb2096
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-08-20 10:23:26 +0900

    ルーティングの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   private
   def after_sign_in_path_for(resource)
-    users_index_path
+    users_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
 
-  namespace :users do
-    get 'index', to: 'users#index'
-    get 'show', to: 'users#show'
-  end
+  resources :users, module: 'users', only: [:index, :show]
 end
