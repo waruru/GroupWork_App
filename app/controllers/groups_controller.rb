f3a6aef91484391b7a6ec75df8756c302359c8a9
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-08-25 17:10:16 +0900

    グループ作成のコント設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,5 +15,16 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
